027ae722096c5cb05558938cc678fa8d71dc8d30
tihomirov.pro <tihomirov.pro@gmail.com>
2019-10-28 14:04:17 +0300

    Update

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,10 +1,11 @@
 // import Swiper from 'swiper/dist/js/swiper'
+// import Swiper from 'swiper'
 import {
   Swiper,
   Navigation,
   Pagination,
   Autoplay
-} from 'swiper/dist/js/swiper.esm.js'
+} from 'swiper/js/swiper.esm.js'
 
 Swiper.use([Navigation, Pagination, Autoplay])
 
@@ -26,17 +27,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const catalogSlider = new Swiper('.main__catalog-container', {
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 1,
     spaceBetween: 30,
     breakpoints: {
-      575: {
-        slidesPerView: 1
-      },
-      991: {
+      576: {
         slidesPerView: 2
       },
-      1200: {
+      992: {
         slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
       }
     },
     autoplay: {
@@ -46,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const reviewsSlider = new Swiper('.reviews__container', {
     loop: true,
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 30,
     centeredSlides: true,
     pagination: {
@@ -55,8 +56,8 @@ document.addEventListener('DOMContentLoaded', () => {
       clickable: true
     },
     breakpoints: {
-      991: {
-        slidesPerView: 1,
+      992: {
+        slidesPerView: 3,
         loopAdditionalSlides: 3
       }
     }
@@ -64,32 +65,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const blogSlider = new Swiper('.blog__container', {
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 1,
     spaceBetween: 30,
     breakpoints: {
       1200: {
+        slidesPerView: 4
+      },
+      992: {
         slidesPerView: 3
       },
-      991: {
+      576: {
         slidesPerView: 2
-      },
-      575: {
-        slidesPerView: 1
       }
     }
   })
 
   const bloggersSlider = new Swiper('.bloggers__slider', {
     loop: true,
-    slidesPerView: 2,
+    slidesPerView: 1,
     spaceBetween: 30,
     speed: 800,
     autoplay: {
       delay: 5000
     },
     breakpoints: {
-      767: {
-        slidesPerView: 1
+      768: {
+        slidesPerView: 2
       }
     }
   })
