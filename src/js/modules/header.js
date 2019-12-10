1a201e97ab47d17e388f6052a76f66283f4527a7
tihomirov.pro <tihomirov.pro@gmail.com>
2019-12-11 01:43:39 +0300

    Update booking, header, buttons

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,15 +1,15 @@
 export function header () {
   const header = document.querySelector('.header')
   const position = window.scrollY
-  if (position > 0) {
+  if (position === 0) {
     header.classList.add('header_bg')
   }
 
   window.addEventListener('scroll', () => {
     const scroll = window.scrollY
-    if (scroll > 0 && !header.classList.contains('header_bg')) {
+    if (scroll === 0 && !header.classList.contains('header_bg')) {
       header.classList.add('header_bg')
-    } else if (scroll === 0) {
+    } else if (scroll > 0) {
       header.classList.remove('header_bg')
     } else {
       return false
