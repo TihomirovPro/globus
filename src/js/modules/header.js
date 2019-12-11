2a9106f68f3b68036905f4e5be3282d5e7cd8dbb
tihomirov.pro <tihomirov.pro@gmail.com>
2019-12-11 13:19:54 +0300

    Update header

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,18 +1,20 @@
 export function header () {
   const header = document.querySelector('.header')
   const position = window.scrollY
-  if (position === 0) {
-    header.classList.add('header_bg')
+
+  if (position > 0 && window.innerWidth > 767) {
+    header.classList.add('header_fixed')
   }
 
-  window.addEventListener('scroll', () => {
-    const scroll = window.scrollY
-    if (scroll === 0) {
-      header.classList.add('header_bg')
-    } else if (scroll > 0) {
-      header.classList.remove('header_bg')
-    } else {
-      return false
-    }
-  })
+  if (window.innerWidth > 767) {
+    window.addEventListener('scroll', () => {
+      const scroll = window.scrollY
+
+      if (scroll !== 0) {
+        header.classList.add('header_fixed')
+      } else {
+        header.classList.remove('header_fixed')
+      }
+    })
+  }
 }
